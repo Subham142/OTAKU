17b9b956d7b3ea2fa58f8988df2c5eecd6621ca2
Subham <subhamy142@gmail.com>
2021-01-24 22:34:54 +0530

    added rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,8 +8,8 @@ const HomeScreens = () => {
             <h1>Latest Products</h1>
             <Row>
                 {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product ={product}/>
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product ={product} />
                     </Col>
                 ))}
             </Row>

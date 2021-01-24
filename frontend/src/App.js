8f8ec3ecb7a73eb7b7eea5eda3e7f72fee3542c2
Subham <subhamy142@gmail.com>
2021-01-24 17:48:29 +0530

    added product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import {Container} from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen'
 const App =() => {
   return (
     <>
     <Header/>
     <main className='py-3'>
       <Container>
-      <h1> Hey !! let's Start </h1>
+      <HomeScreen/>
       </Container>
     </main>
     <Footer/>

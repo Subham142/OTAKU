3f4bfbf3cdb6f4957ab53406ac131ea21eb79591
Subham <subhamy142@gmail.com>
2021-01-28 23:39:28 +0530

    fetch data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import products from '../products'
+//import products from '../products'
 import Product from '../components/Product'
+import axios from 'axios'
 const HomeScreens = () => {
+    const [products,setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchProducts= async() =>{
+            const {data} = await axios.get('/api/products')
+
+            setProducts(data)
+        }
+
+        fetchProducts()
+    },[])
     return (
         <>
             <h1>Latest Products</h1>

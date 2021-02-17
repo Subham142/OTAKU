a6e2faf97c9f65a780e62e0d6ac7983b3a0572f1
Subham <subhamy142@gmail.com>
2021-02-17 18:05:34 +0530

    working in the issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,26 @@
-import {Container} from 'react-bootstrap'
-import { BrowserRouter as Router,Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 
-const App =() => {
+const App = () => {
   return (
     <Router>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-      <Route path="/" component={HomeScreen} exact/>
-      <Route path="/product/:id" component={ProductScreen} />
-      <Route path="/cart/:id?" component={CartScreen} />
-
-
-      </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+        </Container>
+      </main>
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,39 @@
 import React, { useEffect } from 'react'
-import { useDispatch,useSelector } from 'react-redux';
-import { Col, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import loader from '../components/loader'
-import message from '../components/Message'
-import { listProducts } from '../actions/productActions';
-const HomeScreens = () => {
-    const dispatch = useDispatch()
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
 
-    const productList=useSelector(state => state.productList)
-    const {loading,error,products} = productList
+const HomeScreen = () => {
+  const dispatch = useDispatch()
 
-    useEffect(()=>{
-        dispatch(listProducts())
-    },[dispatch])
-    return (
-        <>
-            <h1>Latest Products</h1>
-            {loading ? (<loader/>) : error ?(<message variant ='danger'>{error}</message>) 
-            : <Row>
-            {products.map(product => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product ={product} />
-                </Col>
-            ))}
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
         </Row>
-        }
-            
-        </>
-    )
+      )}
+    </>
+  )
 }
 
-export default HomeScreens
+export default HomeScreen

eda3eaa5afdce42a5312444b703ecb79af5bedb1
Subham <subhamy142@gmail.com>
2021-02-17 16:48:13 +0530

    expi1

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,76 +1,78 @@
 import mongoose from 'mongoose'
 
-const orderSchema = mongoose.Schema({
-    
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'User',
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-    orderItems:[
-        {
-            name:{ type:String, required:true},
-            qty:{ type:Number, required:true},
-            image:{ type:String, required:true},
-            price:{ type:Number, required:true},
-            Product:{
-                type:mongoose.Schema.Types.ObjectId,
-                required:true,
-                ref:'Product',
-            }
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
         },
+      },
     ],
-    shippingAddress:{
-        address:{type:String,required:true},
-        city:{type:String,required:true},
-        postalCode:{type:String,required:true},
-        country:{type:String,required:true},
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
     },
-    paymentMethod:{
-        type: String,
-        required:true,
-     },
-    paymentResult:{
-        id:{type:String},
-        status:{type:String},
-        update_item:{type:String},
-        email_address:{ type:String },
+    paymentMethod: {
+      type: String,
+      required: true,
     },
-    taxPrice:{
-        type:Number,
-        required:true,
-        default:0.0,
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
     },
-    shippingPrice:{
-        type:Number,
-        required:true,
-        default:0.0,
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-    totalPrice:{
-        type:Number,
-        required:true,
-        default:0.0,
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-    isPaid:{
-        type:Boolean,
-        required:true,
-        default:false,
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
     },
-    paidAt:{
-        type:Date,
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-    isDelivered:{
-        type:Boolean,
-        required:true,
-        default:false,
+    paidAt: {
+      type: Date,
     },
-    DeliveredAt:{
-        type:Date,
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
     },
-},{
-    timestamps:true
-})
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-const Order = mongoose.model('Order',orderSchema)
+const Order = mongoose.model('Order', orderSchema)
 
 export default Order
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
@@ -11,46 +12,46 @@ dotenv.config()
 
 connectDB()
 
-const importData = async ()=>{
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        const createdUser = await User.insertMany(users)
-        const adminUser = createdUser[0]._id
+    const createdUsers = await User.insertMany(users)
 
-        const sampleProducts = products.map(product =>{
-            return {...product,user:adminUser}
-        })
+    const adminUser = createdUsers[0]._id
 
-        await Product.insertMany(sampleProducts)
-        console.log('Data Imported!')
-        process.exit()
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
 
-    } catch (error) {
-        console.error(`${error}`)
-        process.exit(1)
-    }
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data Imported!'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
-const destroyData = async ()=>{
-    try {
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        console.log('Data Destroyed!')
-        process.exit()
-
-    } catch (error) {
-        console.error(`${error}`)
-        process.exit(1)
-    }
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
-if(process.argv[2] === '-d'){
-    destroyData()
-}else{
-    importData()
+if (process.argv[2] === '-d') {
+  destroyData()
+} else {
+  importData()
 }

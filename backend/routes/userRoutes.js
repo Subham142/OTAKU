92fd57c9452fff5908e2c581adacda678ee4d569
Subham <subhamy142@gmail.com>
2021-02-20 17:39:02 +0530

    custom authetication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import expres from 'express'
 const router = expres.Router()
-import { authUser } from '../controllers/userController.js'
-
+import { authUser,getUserProfile } from '../controllers/userController.js'
+import { protect } from "../middleware/authMiddleware.js";
 router.post('/login',authUser)
+router.route('/profile').get(protect, getUserProfile)
+
 
 export default router

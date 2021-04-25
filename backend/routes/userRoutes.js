d7ed673e33faf8514740a334182b179363ea771a
Subham <subhamy142@gmail.com>
2021-04-25 09:57:21 +0530

    added google auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,14 +9,16 @@ import {
   deleteUser,
   getUserById,
   updateUser,
-
+  checkUser
 } from '../controllers/userController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
+
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 .put(protect,updateUserProfile)
+router.get('/:email',checkUser)
 router
   .route('/:id')
   .delete(protect, admin, deleteUser)
--- frontend/src/components/googleAuth.js
+++ frontend/src/components/googleAuth.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { Modal } from 'react-bootstrap';
-import { googleOAuth2 } from '../actions/googleAuth';
-const googleAuth = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import axios from "axios";
+import { login,register } from "../actions/userActions";
+import { useDispatch } from "react-redux";
+
+
+const clientId ='641909577010-h792ei2m2sc1ku021ami01smkh8hcs0e.apps.googleusercontent.com';
+
+function Login() {
+
+  const dispatch = useDispatch()
+
+  const onSuccess = async(res) => {
+    const user= res.profileObj;
+    const {name ,email} =user;
+     const password= "12345"; 
+     const {data} = await axios.get(`/api/users/${email}`)
+    
+     console.log(data);
+    if( data.isPresent){
+      dispatch(login(email, password))
+    }else{
+      dispatch(register(name, email, password))
+    }
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+   };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
 }
 
-export default googleAuth
+export default Login;

a8c9a9ff1f1caece6dde16a9b78d2e9a0c1e7753
Subham <subhamy142@gmail.com>
2021-05-01 22:14:57 +0530

    fixed bugs

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome To Otaku',
+  description: 'We sell the best Manga for minimal price',
+  keywords: 'anime, buy manga, cheap manga',
 }
 
 export default Meta
